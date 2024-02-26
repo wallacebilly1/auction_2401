707e04afc26c67a9916177edf45f6e3bf3855181
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:42:07 -0700

    Add test for returning the current highest bid on an item in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -33,4 +33,11 @@ describe Item do
 
     expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
   end
+
+  it 'can return the highest current bid on an item' do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+
+    expect(@item1.current_high_bid).to eq 22
+  end
 end
