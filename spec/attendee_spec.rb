bda4aec9772c69c0d24dd7a5b227abbc77260ddd
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:10:29 -0700

    Add test for returning the name of the attendee

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -12,4 +12,9 @@ describe Attendee do
     expect(attendee).to be_an Attendee
   end
 
+	it 'can return the name of the attendee' do
+    attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    expect(attendee.name).to eq 'Megan'
+  end
+
 end
