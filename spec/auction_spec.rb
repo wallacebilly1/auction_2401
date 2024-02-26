72281359cf5536463276561d0d5273e988a3de23
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 10:00:55 -0700

    Add test for returning potential revenue from the highest bids on all items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -54,4 +54,19 @@ describe Auction do
 
     expect(@auction.unpopular_items).to eq [@item2, @item5]
   end
+
+  it 'returns the sum of the current high bids as the potential revenue' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+
+    expect(@auction.potential_revenue). to eq 87
+  end
 end
