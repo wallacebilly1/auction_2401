96368cfebdcf8d3f11bd3e3e5c358cfc0be65ee3
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:40:25 -0700

    Create add_bid method for item

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,12 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
   end
 
+  def add_bid(attendee, price)
+    @bids[attendee] = price
+  end
 end
