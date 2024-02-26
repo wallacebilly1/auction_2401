3716d9f4d83175701fc8ff4d01ad5f09f4c083b5
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:17:48 -0700

    Add test for adding items to the Auction array in auction_spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,14 +1,25 @@
 require 'pry'
 require 'rspec'
 require './lib/auction'
+require './lib/item'
 
 describe Auction do
   before :each do
-
+    @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
   it 'exists' do
-    auction = Auction.new
-    expect(auction).to be_an Auction
+    expect(@auction).to be_an Auction
+  end
+
+  it 'can add items to an array' do
+    expect(@auction.items).to eq []
+
+    auction.add_item(@item1)
+    auction.add_item(@item2)
+
+    expect(@auction.items).to eq [@item1, @item2]
   end
 end
