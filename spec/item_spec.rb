ab9b5f61b22f1e102dc6e62719d1b8d130113ed0
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:07:08 -0700

    Add test for returning the name of an item in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,6 +8,12 @@ describe Item do
   end
 
   it 'exists' do
-		item1 = Item.new('Chalkware Piggy Bank')
+    item1 = Item.new('Chalkware Piggy Bank')
     expect(item1).to be_an Item
   end
+
+  it 'can return the name of the item' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    expect(item1.name).to eq 'Chalkware Piggy Bank'
+  end
+end
