356258feb43be8eec1015ba4a2e8ec2d00a4864e
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:58:30 -0700

    Update setup for unpopular items test in auction_spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -38,11 +38,16 @@ describe Auction do
   end
 
   it 'can return any unpopular items in the auction' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    
     @item1.add_bid(@attendee2, 20)
     @item1.add_bid(@attendee1, 22)
     @item4.add_bid(@attendee3, 50)
 
     expect(@auction.unpopular_items).to eq [@item2, @item3, @item5]
   end
-
 end
