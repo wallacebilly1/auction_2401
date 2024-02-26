6dd7909a0d35a5fd04d5c4777dd552467d54fbcd
Billy Wallace <wallace.billy1@gmail.com>
2024-02-26 09:19:08 -0700

    Create add_items method and added items to attr_reader in auction

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,3 +1,12 @@
 class Auction
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
 
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -17,8 +17,8 @@ describe Auction do
   it 'can add items to an array' do
     expect(@auction.items).to eq []
 
-    auction.add_item(@item1)
-    auction.add_item(@item2)
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
 
     expect(@auction.items).to eq [@item1, @item2]
   end
